b2fb1cc702897e579b0647541eec4e091435f542
Sunil Joshi <joshisunil2601@gmail.com>
2022-08-16 20:30:24 +0530

    change code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Board from "./components/Board";
 function App() {
   return (
     <div >
-      <h1 className=" heading">Tic Tac Toe</h1>
+  
       <h1 className=" heading">Tic Tac Toe</h1>
       <Board/>
     </div>
